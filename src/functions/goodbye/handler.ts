c06741e2866d441b644d18c08a8cdf2746e8a893
Chris Lemler <clemler@pm.me>
2021-01-20 08:47:18 -0800

    Added more greetings

--- src/functions/goodbye/handler.ts
+++ src/functions/goodbye/handler.ts
@@ -1,12 +1,48 @@
 /* eslint-disable arrow-body-style */
 import { APIGatewayProxyResult, APIGatewayProxyEvent } from 'aws-lambda';
+import AWS from 'aws-sdk';
 import response from '../../lib/response';
 
-// export default async handler = async (): Promise<APIGatewayProxyResult> => {
-//   return { body: JSON.stringify({ message: 'hello' }), statusCode: 200 };
-// };
-
 export default async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//  return Promise.resolve({ body: JSON.stringify({ message: 'goodbye' }), statusCode: 200 });
-  return Promise.resolve(response.success(200, {}, {message: 'goodbye'}));
+  console.log('Entered /goodbye lambda function');
+
+  // Change the connection to DynamoDB if we are running locally
+  AWS.config.update({
+    region: 'us-west-2',
+    logger: console,
+    endpoint: 'http://dynamodb:8000',
+    httpOptions: {
+      connectTimeout: 200
+    },
+    maxRetries: 3
+  }, true);
+
+  console.log('Creating the DocumentClient');
+  let dynamodb = new AWS.DynamoDB.DocumentClient();
+  const table = 'greetings-db';
+  const params = {
+    TableName: table,
+    Key:{
+        "id": "goodbye",
+        "lang": "de"
+    }
+  };
+
+  console.log('Calling dynamodb.get()')
+  let results: any = {};
+  try {
+    results = await dynamodb.get(params).promise();
+    if ( !results.hasOwnProperty('Info')) {
+      console.log(`${params.Key} not found`);
+      return Promise.resolve(response.error(400, {}));
+    }
+    console.log(results);
+  } catch (err) {
+    console.log('Uh oh entered the CATCH block');
+    console.log(err);
+    return Promise.resolve(response.error(500, {}));
+  }
+
+  //await new Promise(r => setTimeout(r, 5000));
+  return Promise.resolve(response.success(200, {}, {message: results.Item!.greeting}));
 };
--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -4,32 +4,30 @@ import { APIGatewayProxyResult, APIGatewayProxyEvent } from 'aws-lambda';
 import AWS from 'aws-sdk';
 import response from '../../lib/response';
 
-// export default async handler = async (): Promise<APIGatewayProxyResult> => {
-//   return { body: JSON.stringify({ message: 'hello' }), statusCode: 200 };
-// };
-
 export default async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Entered /hello lambda function');
 
   // Change the connection to DynamoDB if we are running locally
-  // AWS.config.update({
+  AWS.config.update({
+    region: 'us-west-2',
+    logger: console,
+    endpoint: 'http://dynamodb:8000',
+    httpOptions: {
+      connectTimeout: 200
+    },
+    maxRetries: 3
+  }, true);
+
+  console.log('Creating the DocumentClient');
+  // let dynamodb = new AWS.DynamoDB.DocumentClient({
   //   region: 'us-west-2',
+  //   endpoint: 'http://dynamodb:8000',
   //   logger: console,
-  //   endpoint: 'http://localhost:8000',
   //   httpOptions: {
   //     connectTimeout: 500
   //   }
-  // }, true);
-
-  console.log('Creating the DocumentClient');
-  let dynamodb = new AWS.DynamoDB.DocumentClient({
-    region: 'us-west-2',
-    endpoint: 'http://dynamodb:8000',
-    logger: console,
-    httpOptions: {
-      connectTimeout: 500
-    }
-  });
+  // });
+  let dynamodb = new AWS.DynamoDB.DocumentClient();
   const table = 'greetings-db';
   const params = {
     TableName: table,
@@ -39,35 +37,17 @@ export default async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResul
     }
   };
 
-  dynamodb.get(params, (err, data) => {
-    if (err) {
-      console.log('Error from Dynamodb');
-      console.log(err);
-    } else {
-      console.log('Yeah we got data');
-      console.log(data);
-    }
-  });
+  console.log('Calling dynamodb.get()')
+  let results;
+  try {
+    results = await dynamodb.get(params).promise();
+    console.log(results);
+  } catch (err) {
+    console.log('Uh oh entered the CATCH block');
+    console.log(err);
+    return Promise.resolve(response.error(500, {}));
+  }
 
-  // console.log('Calling dynamodb.get()')
-  // try {
-  //   const results = await dynamodb.get(params).promise();
-  //   console.log(results);
-  // } catch (err) {
-  //   console.log('Uh oh entered the CATCH block');
-  //   console.log(err);
-  //   return Promise.resolve(response.error(500, {}));
-  // }
-
-
-  // dynamodb.get(params).promise()
-  // .then((data) => {
-  //   console.log(data);
-  //   return Promise.resolve(response.success(200, {}, {message: 'hello'}));
-  // }).catch((err) => {
-  //   console.log(err);
-  //   return Promise.resolve(response.error(500, {}));
-  // });
-  await new Promise(r => setTimeout(r, 5000));
-  return Promise.resolve(response.success(200, {}, {message: 'hello'}));
+  //await new Promise(r => setTimeout(r, 5000));
+  return Promise.resolve(response.success(200, {}, {message: results.Item!.greeting}));
 };
